5174894ebe6ee258557343b0ce47c7341c4e523c
OussamaDjebbour <oussama_djebbour@outlook.com>
2023-12-29 16:53:57 +0100

    deployement

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import { useState } from "react";
 import Image from "./components/Image";
 import Box from "./components/Box";
-
-function calculateWinner(boxes) {
-  const winningPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < winningPatterns.length; i++) {
-    const [a, b, c] = winningPatterns[i];
-    if (boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c])
-      return boxes[a];
-  }
-  return null;
-}
+import calculateWinner from "./calculateWinner";
 
 function App() {
   const [boxes, setBoxes] = useState(Array(9).fill(null));
   const [isX, setIsX] = useState(true);
+
+  const winner = calculateWinner(boxes);
 
   function handleClick(i) {
     if (boxes[i] || calculateWinner(boxes)) return;
@@ -37,8 +20,6 @@ function App() {
     setBoxes((boxes) => boxes.map((box) => null));
     setIsX(true);
   }
-
-  const winner = calculateWinner(boxes);
 
   return (
     <div className="app">
